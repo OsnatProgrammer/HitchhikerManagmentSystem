70c2cfe014e5a545bdbad2a3c0e73a73540bbae5
HadassahAckerman <116038042+HadassahAckerman@users.noreply.github.com>
2023-06-12 15:14:46 +0300

    changes

--- models/rideDetailModel.js
+++ models/rideDetailModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const rideDetailsSchema = new mongoose.Schema({
-    ride_details_id: String,
+  
     departure_address: String,
     destination_address: String,
     departure_time:Date,
@@ -14,7 +14,6 @@ exports.rideDetailsSchema = mongoose.model("rideDetails", rideDetailsSchema);
 
 exports.validateRideDetails = (_reqBody) => {
     let schemaJoi = Joi.object({
-        ride_details_id: Joi.string().required(),
         departure_address: Joi.string().min(5).max(200).required(),
         destination_address: Joi.string().min(5).max(200).required(),
         departure_time: Joi.Date().required(),
