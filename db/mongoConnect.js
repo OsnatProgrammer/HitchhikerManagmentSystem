a2b57dd1796db4c6955fd8caccfc2cf6a0eca05d
michal shunem <michal80996@gmail.com>
2023-06-26 20:27:06 +0300

    change "admin" in authadmin

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -10,3 +10,6 @@ async function main() {
     console.log("mongo connect HitchhikerManagmentSystem");
 }
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+
+    // mongodb+srv://hadassahdevelop:bMHyhsl4Yuwj4Hgs@cluster0.pa1epkq.mongodb.net/
